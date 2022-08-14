c783856baa6f4811e384618e01785f4998cf8260
pulchnymamut <pulchnymamut@gmail.com>
2022-08-14 19:46:04 +0200

    Added separate footer menu

--- footer.php
+++ footer.php
@@ -1,15 +1,22 @@
 
 <footer class="footer">
-        <?php wp_nav_menu(
-                array(
-                        'menu' => 'footer',
-                        'container' => '',
-                        'theme_location' => 'footer',
-                        'items_wrap' => '<div id="footer-menu" class="footer-menu">%3$s</div>',
-                        'container_aria_label' => 'footer-menu'
-                )
-        )?>
+        <div class="footer-content">
+                <?php wp_nav_menu(
+                        array(
+                                'menu' => 'footer',
+                                'container' => '',
+                                'theme_location' => 'footer',
+                                'items_wrap' => '<div id="footer-menu" class="footer-menu">%3$s</div>',
+                                'container_aria_label' => 'footer-menu'
+                        )
+                )?>
+                <div class="socials-wrapper">
+                        <p id="footer-s-title">SOCIAL MEDIA</p>
+                        <?php get_template_part('template-parts/socials'); ?>
+                </div>
+        </div>
+        
+        
 </footer>
-
- <?php wp_footer(); ?>
+<?php wp_footer(); ?>
 </body>
