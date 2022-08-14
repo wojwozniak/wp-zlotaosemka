30a70559223c4124d5677e4b5c242c738849e265
pulchnymamut <pulchnymamut@gmail.com>
2022-08-14 19:46:21 +0200

    Added socials tempate part

---
+++ template-parts/socials.php
@@ -0,0 +1,5 @@
+<ul class="socials">
+    <li><a href="" target="_blank"><i class="fa-brands fa-facebook"></i></a></li>
+    <li><a href="" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
+    <li><a href="" target="_blank"><i class="fa-brands fa-youtube"></i></a></li>
+</ul>  
