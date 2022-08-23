fb75e83e5a9a3885358cf6a02010d35ee8f8c44f
pulchnymamut <pulchnymamut@gmail.com>
2022-08-23 17:51:52 +0200

    Each jednostka page now displays posts from category with same name as pages' short url

--- templates/druzyna.php
+++ templates/druzyna.php
@@ -70,7 +70,22 @@
 	</article>
 	<article id="articles" class="site">
 		<h2>Aktualności</h2>
-		
+		<?php                
+                $args = array(
+                    'post_type' => 'post',
+                    'post_status' => 'publish',
+                    'category_name' => basename(get_page_link()),
+                    'posts_per_page' => 10,
+                );
+                $arr_posts = new WP_Query( $args );
+                if ( $arr_posts->have_posts() ) :
+                    while ( $arr_posts->have_posts() ) :
+                        $arr_posts->the_post();
+						get_template_part('template-parts/content', 'archive');
+                    endwhile;
+                endif;
+			?>
+			<?php the_posts_pagination(); ?>
 	</article>
 </div>
 
