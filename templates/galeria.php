b76434b162eccee471505c7eaafd4ae8e03c4b26
pulchnymamut <pulchnymamut@gmail.com>
2022-09-15 15:34:37 +0200

    Update pre-review

--- templates/galeria.php
+++ templates/galeria.php
@@ -3,40 +3,44 @@
 	get_header();
 ?>
 
-<div class="single-wrapper">
-	<article class="article">
-		<div id="page-wrapper">
-			<div id="buttons">
-				<button id="2022" class="galbtn btnstyle currentbtn">2022</button>
-				<div class="vl"></div>
-				<button id="2021" class="galbtn btnstyle">2021</button>
-				<div class="vl"></div>
-				<button id="2020" class="galbtn btnstyle">2020</button>
-				<div class="vl"></div>
-				<button id="2019" class="galbtn btnstyle">2019</button>
-				<div class="vl"></div>
-				<button id="2018" class="galbtn btnstyle">2018</button>
-			</div> <!-- End of buttons div -->
-			<div class="gallery-container">
-				<div id="g-2022" class="k-past-container gal displaygal">
-					<div class="k-past">
-						<a href="https://photos.google.com/share/AF1QipO7fBDHcBFhvcyS32_x4sfRGvF8l_ioQXUKX6pztW-c6ZvJ4NmXYL32GAth61Jf4g?key=bWNuSUJxRHctWGNMMmRFSEJITFVqOU5nbmVteFVR" target="_blank">
-							<p>Harcerska Akcja Letnia</p>
-							<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/example.jpg" alt="">
-						</a>
-					</div> <!-- End of single gal -->
-				</div> <!-- End of year container -->
-				<div id="g-2021" class="k-past-container gal">
-					<div class="k-past">
-						<a href="https://photos.google.com/share/AF1QipO7fBDHcBFhvcyS32_x4sfRGvF8l_ioQXUKX6pztW-c6ZvJ4NmXYL32GAth61Jf4g?key=bWNuSUJxRHctWGNMMmRFSEJITFVqOU5nbmVteFVR" target="_blank">
-							<p>Test 2</p>
-							<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/example.jpg" alt="">
-						</a>
-					</div> <!-- End of single gal -->
-				</div> <!-- End of year container -->
+<img id="site-bg" style="background-image: url("<?php the_post_thumbnail('thumbnail',['class' => 'site-bg', 'title' => 'Tło']); ?>")></img>
+<h1 id="title"><?php wp_title('') ?></h1>
+<div id="site-root">
+	<div class="single-wrapper">
+		<article class="article">
+			<div id="page-wrapper">
+				<div id="galbuttons">
+					<button id="2022" class="galbtn btnstyle currentbtn">2022</button>
+					<div class="vl"></div>
+					<button id="2021" class="galbtn btnstyle">2021</button>
+					<div class="vl"></div>
+					<button id="2020" class="galbtn btnstyle">2020</button>
+					<div class="vl"></div>
+					<button id="2019" class="galbtn btnstyle">2019</button>
+					<div class="vl"></div>
+					<button id="2018" class="galbtn btnstyle">2018</button>
+				</div> <!-- End of buttons div -->
+				<div class="gallery-container">
+					<div id="g-2022" class="k-past-container gal displaygal">
+						<div class="k-past">
+							<a href="https://photos.google.com/share/AF1QipO7fBDHcBFhvcyS32_x4sfRGvF8l_ioQXUKX6pztW-c6ZvJ4NmXYL32GAth61Jf4g?key=bWNuSUJxRHctWGNMMmRFSEJITFVqOU5nbmVteFVR" target="_blank">
+								<p>Harcerska Akcja Letnia</p>
+								<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/example.jpg" alt="">
+							</a>
+						</div> <!-- End of single gal -->
+					</div> <!-- End of year container -->
+					<div id="g-2021" class="k-past-container gal">
+						<div class="k-past">
+							<a href="https://photos.google.com/share/AF1QipO7fBDHcBFhvcyS32_x4sfRGvF8l_ioQXUKX6pztW-c6ZvJ4NmXYL32GAth61Jf4g?key=bWNuSUJxRHctWGNMMmRFSEJITFVqOU5nbmVteFVR" target="_blank">
+								<p>Test 2</p>
+								<img class="k-past-img" src="<?php echo get_template_directory_uri(); ?>/assets/images/example.jpg" alt="">
+							</a>
+						</div> <!-- End of single gal -->
+					</div> <!-- End of year container -->
+				</div>
 			</div>
-		</div>
-	</article>
+		</article>
+	</div>
 </div>
 
 
--- templates/sponsor.php
+++ templates/sponsor.php
@@ -3,19 +3,27 @@
 	get_header();
 ?>
 
-<div class="single-wrapper">
-	<article class="article">
-		<div id="page-wrapper">
-            <div class="sponsor-container">
-                <div class="sponsor">
-                    <a href="https://photos.google.com/share/AF1QipO7fBDHcBFhvcyS32_x4sfRGvF8l_ioQXUKX6pztW-c6ZvJ4NmXYL32GAth61Jf4g?key=bWNuSUJxRHctWGNMMmRFSEJITFVqOU5nbmVteFVR" target="_blank">
-                        <p>Harcerska Akcja Letnia</p>
-                        <img class="sponsor-logo" src="https://www.facebook.com/images/fb_icon_325x325.png" alt="">
-                    </a>
+<img id="site-bg" style="background-image: url("<?php the_post_thumbnail('thumbnail',['class' => 'site-bg', 'title' => 'Tło']); ?>")></img>
+<h1 id="title"><?php wp_title('') ?></h1>
+<div id="site-root">
+    <div class="single-wrapper">
+        <article class="article">
+            <div id="page-wrapper">
+                <div class="sponsor-container">
+                    <div class="sponsor">
+                        <a href="https://photos.google.com/share/AF1QipO7fBDHcBFhvcyS32_x4sfRGvF8l_ioQXUKX6pztW-c6ZvJ4NmXYL32GAth61Jf4g?key=bWNuSUJxRHctWGNMMmRFSEJITFVqOU5nbmVteFVR" target="_blank">
+                            <img class="sponsor-logo" src="https://www.facebook.com/images/fb_icon_325x325.png" alt="">
+                        </a>
+                    </div>
+                    <div class="sponsor">
+                        <a href="https://photos.google.com/share/AF1QipO7fBDHcBFhvcyS32_x4sfRGvF8l_ioQXUKX6pztW-c6ZvJ4NmXYL32GAth61Jf4g?key=bWNuSUJxRHctWGNMMmRFSEJITFVqOU5nbmVteFVR" target="_blank">
+                            <img class="sponsor-logo" src="https://www.slawkow.pl/uploads/important_files/obrazekog.png" alt="">
+                        </a>
+                    </div>
                 </div>
             </div>
-		</div>
-	</article>
+        </article>
+    </div>
 </div>
 
 
