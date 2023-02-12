95de8aaa729a0f59a6b3e7f7c4d2bd3a4f2d465b
Wojtek <pulchnymamut@gmail.com>
2023-02-12 14:40:24 +0100

    update 12.02.2023

--- functions.php
+++ functions.php
@@ -1,6 +1,5 @@
 <?php
 
-
 function zlotaosemka_theme_support() {
 	add_theme_support('title-tag');
 	add_theme_support('custom-logo');
@@ -31,17 +30,44 @@ function zlotaosemka_register_styles() {
 
 add_action('wp_enqueue_scripts', 'zlotaosemka_register_styles');
 
+function my_custom_date_format($format) {
+  return 'd.m.Y';
+}
+
+
 function zlotaosemka_register_scripts() {
 
 	wp_enqueue_script('zlotaosemka-jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true);
 	wp_enqueue_script('zlotaosemka-popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true);
 	wp_enqueue_script('zlotaosemka-main', get_template_directory_uri() .'/assets/js/main.js', array(), '1.0', true);
 	if (is_front_page() == true) {
+		add_filter( 'mc_date_format', 'my_custom_date_format' );
 		wp_enqueue_script('pustynnaburza-frontpage-nav', get_template_directory_uri() .'/assets/js/frontnav.js', array(), '1.0', true);
 	} else {
 		wp_enqueue_script('pustynnaburza-other-nav', get_template_directory_uri() .'/assets/js/other.js', array(), '1.0', true);
 	}
-
+	add_filter( 'lazyblock/galeria-przerwa-miedzy-przyciskami/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/galeria-przyciski-start/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/galeria-koniec-przyciskow/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/galeria-przycisk/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/koniec-galerii/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/galeria-koniec-roku/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/galeria-blok/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/galeria-poczatek-roku/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/galeria-techniczne-poczatek/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/druzyna-poczatek-przyciskow/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/druzyna-przycisk/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/druzyna-przerwa-miedzy-przyciskami/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/druzyna-koniec-przyciskow/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/druzyna-poczatek-contentu/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/druzyna-poczatek-kontenera/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/druzyna-tekst-w-kontenerze/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/druzyna-zdjecie-w-kontenerze/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/zdjecie-osoby-z-opisem/frontend_allow_wrapper', '__return_false' );
+		add_filter( 'lazyblock/zdjecie-osoby-z-opisem-szef/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/druzyna-koniec-kontenera/frontend_allow_wrapper', '__return_false' );
+	add_filter( 'lazyblock/druzyna-koniec-contentu/frontend_allow_wrapper', '__return_false' );
+		add_filter( 'lazyblock/dla-mediow-plik/frontend_allow_wrapper', '__return_false' );
 }
 
 add_action('wp_enqueue_scripts', 'zlotaosemka_register_scripts');
--- index.php
+++ index.php
@@ -12,13 +12,13 @@
 		<article class="sidebar-element">
 			<h2>KALENDARZ</h2>
 			<p>
-				Brak nadchodzących wydarzeń
+				<?php echo do_shortcode('[my_calendar format="list"]'); ?>
 			</p>
 		</article>
 		<article class="sidebar-element logos">
-		<img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/i1.jpg' ); ?>" alt="" class="sidebar-logo">
-		<img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/i2.png' ); ?>" alt="" class="sidebar-logo">
-		<img id="logo-hufiec" src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/i3.jpg' ); ?>" alt="" class="sidebar-logo">
+			<a href="https://zhp.pl/" target="_blank" style="width: 250px"><img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/i1.jpg' ); ?>" alt="" class="sidebar-logo"></a>
+			<a href="https://slaska.zhp.pl/" target="_blank" style="width: 250px"><img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/i2.png' ); ?>" alt="" class="sidebar-logo"></a>
+			<a href="https://dabrowagornicza.zhp.pl/" target="_blank" style="width: 200px"><img id="logo-hufiec" src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/i3.jpg' ); ?>" alt="" class="sidebar-logo"></a>
 		</article>
 	</aside>
 	<article class="wrapper">
@@ -39,7 +39,14 @@
                             endwhile;
                         endif;
                     ?>
-			<?php the_posts_pagination(); ?>
+			<?php the_posts_pagination(array(
+				'mid_size'           => 1,
+				'prev_text'          => _x( 'Poprzednia strona', 'Poprzednia strona z postami' ),
+				'next_text'          => _x( 'Następna strona', 'Następna strona z postami' ),
+				'screen_reader_text' => __( 'Wybór strony' ),
+				'aria_label'         => __( 'Posts' ),
+				'class'              => 'pagination',
+			)); ?>
 		</div>
 	</article>
 </div>
