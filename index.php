e9cfb76b2ff66db4d7c27b861a9cc42f915fbf76
Wojtek <pulchnymamut@gmail.com>
2024-11-15 23:16:16 +0100

    linki do sidebaru strony głównej, wyrównanie loga

--- index.php
+++ index.php
@@ -13,10 +13,15 @@
 			<h2>KALENDARZ</h2>
 			<?php echo do_shortcode('[my_calendar category="1" months="12" weekends="true" format="list"]'); ?>
 		</article>
+		 <article class="sidebar-element sidebar-socials">
+			 <h2>ŚLEDŹ NASZE DZIAŁANIA</h2>
+			 <a href="https://www.youtube.com/c/ZlotaOsemka" target="_blank" class="s-link"><i class="fa-brands fa-youtube"></i><p>&nbsp;Kanał YouTube Związku Drużyn</p></a>
+			 <a href="https://www.facebook.com/zlotaosemka" target="_blank" class="s-link fb-link"><i class="fa-brands fa-facebook-f dont-show"></i>&nbsp;<p class="fb-link-text"></p></a>
+         </article>
 		<article class="sidebar-element logos">
 			<a href="https://zhp.pl/" target="_blank" style="width: 250px"><img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/i1.jpg' ); ?>" alt="" class="sidebar-logo"></a>
 			<a href="https://slaska.zhp.pl/" target="_blank" style="width: 250px"><img src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/i2.png' ); ?>" alt="" class="sidebar-logo"></a>
-			<a href="https://dabrowagornicza.zhp.pl/" target="_blank" style="width: 200px"><img id="logo-hufiec" src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/i3.jpg' ); ?>" alt="" class="sidebar-logo"></a>
+			<a href="https://dabrowagornicza.zhp.pl/" target="_blank" style="width: 250px; display:flex;"><img id="logo-hufiec" src="<?php echo esc_url( get_template_directory_uri() . '/assets/images/i3.jpg' ); ?>" alt="" class="sidebar-logo"></a>
 		</article>
 	</aside>
 	<article class="wrapper">
@@ -27,7 +32,7 @@
                             'post_type' => 'post',
                             'post_status' => 'publish',
                             'category_name' => 'main',
-                            'posts_per_page' => 10,
+                            'posts_per_page' => 6,
 							'paged'          => get_query_var( 'paged' ),
                         );
                         $arr_posts = new WP_Query( $args );
