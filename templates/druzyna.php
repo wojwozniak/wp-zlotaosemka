ebc9434729d34e2094faf0f553ff2068f7b3c08b
Wojtek <pulchnymamut@gmail.com>
2024-11-18 00:15:50 +0100

    pagination on team pages

--- templates/druzyna.php
+++ templates/druzyna.php
@@ -1,10 +1,9 @@
 <?php
-	/* Template Name: Drużyna/Gromada/Jednostka */
-	get_header();
+    /* Template Name: Drużyna/Gromada/Jednostka */
+    get_header();
 ?>
-<img id="site-bg" style="background-image: url("<?php the_post_thumbnail('large',['class' => 'site-bg', 'title' => 'Tło']); ?>"></img>
-																															   
-																															   
+<img id="site-bg" style="background-image: url('<?php echo esc_url(get_the_post_thumbnail_url(get_the_ID(), 'large')); ?>');"></img>
+
 <?php
     $post_id = get_the_ID();
     $custom_title = get_post_meta($post_id, 'custom_title', true);
@@ -29,49 +28,63 @@
             echo '</div>';
         }
     ?>
-	<article class="site">
-		<?php echo the_content(); ?>
-	</article>
-        <div class="lower">
-            <aside class="sidebar">
-                <article class="sidebar-element">
-                    <h2>KALENDARZ</h2>
-					<?php echo do_shortcode('[my_calendar category="1" months="12" weekends="true" format="list"]'); ?>
-                </article>
-                <article class="sidebar-element sidebar-socials">
-                    <h2>SOCIAL MEDIA</h2>
-					<a href="https://www.youtube.com/c/ZlotaOsemka" target="_blank" class="s-link"><i class="fa-brands fa-youtube"></i><p>&nbsp;Kanał YouTube Związku Drużyn</p></a>
-					<a href="" target="_blank" class="s-link fb-link"><i class="fa-brands fa-facebook-f dont-show"></i>&nbsp;<p class="fb-link-text"></p></a>
-					<a href="" target="_blank" class="s-link ig-link"><i class="fa-brands fa-instagram dont-show"></i>&nbsp;<p class="ig-link-text"></p></a>
-                </article>
-            </aside>
-            <article id="articles">
-                <h2>AKTUALNOŚCI</h2>
-                <?php                
-                        $args = array(
-                            'post_type' => 'post',
-                            'post_status' => 'publish',
-                            'category_name' => basename(get_page_link()),
-                            'posts_per_page' => 50,
-							'paged'          => get_query_var( 'paged' ),
-                        );
-                        $arr_posts = new WP_Query( $args );
-                        if ( $arr_posts->have_posts() ) :
-                            while ( $arr_posts->have_posts() ) :
-                                $arr_posts->the_post();
-                                get_template_part('template-parts/content', 'archive');
-                            endwhile;
-                        endif;
-                    ?>
+    <article class="site">
+        <?php the_content(); ?>
+    </article>
+    <div class="lower">
+        <aside class="sidebar">
+            <article class="sidebar-element">
+                <h2>KALENDARZ</h2>
+                <?php echo do_shortcode('[my_calendar category="1" months="12" weekends="true" format="list"]'); ?>
             </article>
-			<?php the_posts_pagination(array(
-				'mid_size'           => 1,
-				'prev_text'          => _x( 'Poprzednia strona', 'previous set of posts' ),
-				'next_text'          => _x( 'Następna strona', 'next set of posts' ),
-				'screen_reader_text' => __( 'Wybór strony' ),
-				'aria_label'         => __( 'Posts' ),
-				'class'              => 'pagination',
-			)); ?>
-   </div>
+            <article class="sidebar-element sidebar-socials">
+                <h2>SOCIAL MEDIA</h2>
+                <a href="https://www.youtube.com/c/ZlotaOsemka" target="_blank" class="s-link">
+                    <i class="fa-brands fa-youtube"></i><p>&nbsp;Kanał YouTube Związku Drużyn</p>
+                </a>
+                <a href="" target="_blank" class="s-link fb-link">
+                    <i class="fa-brands fa-facebook-f dont-show"></i>&nbsp;<p class="fb-link-text"></p>
+                </a>
+                <a href="" target="_blank" class="s-link ig-link">
+                    <i class="fa-brands fa-instagram dont-show"></i>&nbsp;<p class="ig-link-text"></p>
+                </a>
+            </article>
+        </aside>
+        <article id="articles">
+            <h2>AKTUALNOŚCI</h2>
+            <?php
+                $args = array(
+                    'post_type'      => 'post',
+                    'post_status'    => 'publish',
+                    'category_name'  => basename(get_page_link()),
+                    'posts_per_page' => 5,
+                    'paged'          => max(1, get_query_var('paged', 1)),
+                );
+                $arr_posts = new WP_Query($args);
+                if ($arr_posts->have_posts()) :
+                    while ($arr_posts->have_posts()) :
+                        $arr_posts->the_post();
+                        get_template_part('template-parts/content', 'archive');
+                    endwhile;
+			
+				echo '<nav class="navigation pagination" aria-label="Wpisy">';
+				echo '<h2 class="screen-reader-text">Wybór strony</h2>';
+				echo '<div class="nav-links">';
+
+				echo paginate_links(array(
+					'total'        => $arr_posts->max_num_pages,
+					'current'      => max(1, get_query_var('paged', 1)),
+					'mid_size'     => 1,
+					'prev_text'    => __('Poprzednia strona'),
+					'next_text'    => __('Następna strona'),
+					'before_page_number' => '<span class="custom-page-number">',
+					'after_page_number'  => '</span>',
+				));
+				wp_reset_postdata();
+                endif;
+			echo '</div></nav>';
+            ?>
+        </article>
+    </div>
 </div>
 <?php get_footer(); ?>
