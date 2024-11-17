3425c709948eaac14327a8c0f54cbc6081a20540
Wojtek <pulchnymamut@gmail.com>
2024-11-17 20:53:55 +0100

    allow for custom title subtitle

--- templates/sponsor.php
+++ templates/sponsor.php
@@ -4,7 +4,22 @@
 ?>
 
 <img id="site-bg" style="background-image: url("<?php the_post_thumbnail('large',['class' => 'site-bg', 'title' => 'Tło']); ?>")></img>
-<div id="title"><h1 class="title-part"><?php wp_title('') ?></h1></div>
+<div id="title">
+	<?php
+		$post_id = get_the_ID();
+		$custom_title = get_post_meta($post_id, 'custom_title', true);
+		$custom_subtitle = get_post_meta($post_id, 'custom_subtitle', true);
+
+	if (!empty($custom_title)) {
+		echo '<h1 class="cus-theme-title">' . esc_html($custom_title) . '</h1>';
+	} else {
+		echo '<h1 class="cus-theme-title">' . wp_title('', false) . '</h1>';
+	}
+	if (!empty($custom_subtitle)) {
+    echo '<h2 class="cus-theme-subtitle">' . esc_html($custom_subtitle) . '</h2>';
+	}
+	?>
+</div>
 <div id="site-root">
     <div class="single-wrapper">
         <article class="article">
